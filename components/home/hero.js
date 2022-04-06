af2470c2a20e8c68a208d1168196d799a777a88c
Emmanuel Garcia <earturoga@gmail.com>
2022-04-05 22:24:42 -0600

    Added a media query for small screens

--- components/home/hero.js
+++ components/home/hero.js
@@ -15,39 +15,44 @@ function Hero() {
       </div>
       <h1>Hi, I'm Emmanuel</h1>
       <p>
-        I am web developer from El Paso TX, welcome to my personal website.
-        I am open for new opportunities, please contact me in my contact page or through LinkedIn.
+        I am web developer from El Paso TX, welcome to my personal website. I am
+        open for new opportunities, please contact me in my contact page or
+        through LinkedIn.
       </p>
       <h2>-Tech Stack-</h2>
       <div className={classes.hero}>
         <span>
           <Image
             src="/static/js.png"
-            width={100}
-            height={100}
+            width="100%"
+            height="100%"
             alt="Javascript logo"
           />
         </span>
         <span>
           <Image
             src="/static/react.png"
-            width={100}
-            height={100}
+            width="100%"
+            height="100%"
             alt="React logo"
           />
         </span>
-        <Image
-          src="/static/mongodb.png"
-          width={100}
-          height={100}
-          alt="Mongo Database logo"
-        />
-        <Image
-          src="/static/nodejs.png"
-          width={100}
-          height={100}
-          alt="Node.js logo"
-        />
+        <span>
+          <Image
+            src="/static/mongodb.png"
+            width="100%"
+            height="100%"
+            alt="Mongo Database logo"
+          />
+        </span>
+        <span>
+          <Image
+            src="/static/nodejs.png"
+            width="100%"
+            height="100%"
+            alt="Node.js logo"
+          />
+        </span>
       </div>
     </section>
   );
