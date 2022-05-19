e89f8938e37e7a2db10a72a9b1cfc22ab53a54c3
Emmanuel Garcia <earturoga@gmail.com>
2022-05-19 00:35:12 -0600

    added hamburger menu

--- components/layout/mainNavigation.js
+++ components/layout/mainNavigation.js
@@ -1,29 +1,98 @@
 import Logo from "./logo";
 import Link from "next/link";
-import classes from "./mainNavigation.module.css"
+import { useRouter } from "next/router";
+import classes from "./mainNavigation.module.css";
+import { useState, useEffect } from "react";
 
 function MainNavigation() {
-  return (
-      <header className={classes.header}>
-        <Link href="/">
-          <a>
-            <Logo />
-          </a>
-        </Link>
-        <nav className={classes.nav}>
-          <ul>
-            <li className="navItem">
-              <Link href="/posts">Posts</Link>
+  const [width, setWidth] = useState(null);
+  const [sideBar, setSideBar] = useState(false);
+  const router = useRouter();
+
+  const breakpoint = 500;
+  useEffect(() => {
+    if (width === null) {
+      setWidth(window.innerWidth);
+    }
+    const handleWindowResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleWindowResize);
+    return () => window.removeEventListener("resize", handleWindowResize);
+  }, []);
+  function handleSideBar() {
+    setSideBar((prev) => !prev);
+  }
+  function handleMobile(path) {
+    () => handleSideBar();
+    router.push(path);
+  }
+  const desktop = (
+    <ul>
+      <li>
+        <Link href="/posts">Posts</Link>
+      </li>
+      <li>
+        <Link href="/apps">Apps</Link>
+      </li>
+      <li>
+        <Link href="/contact">Contact</Link>
+      </li>
+    </ul>
+  );
+  const mobile = (
+    <div className={classes.flexMobileRight}>
+      <ul className={classes.navListMobile}>
+        <li className={classes.hamburger} onClick={handleSideBar}>
+          <img
+            src="./static/menu.png"
+            alt="menu icon, click to expand"
+            height={24}
+            width={24}
+          />
+        </li>
+        <aside
+          className={sideBar ? classes.activeSide : classes.side}
+          onClick={handleSideBar}
+        >
+          <ul className={classes.drawer}>
+            <li
+              className={classes.navItem}
+              onClick={() => handleMobile("/")}
+            >
+              Home
             </li>
-            <li className="navItem">
-            <Link href="/apps">My Apps</Link>
-          </li>
-            <li className="navItem">
-              <Link href="/contact">Contact</Link>
+            <li
+              className={classes.navItem}
+              onClick={() => handleMobile("posts")}
+            >
+              Posts
+            </li>
+            <li
+              className={classes.navItem}
+              onClick={() => handleMobile("apps")}
+            >
+              Apps
+            </li>
+            <li
+              className={classes.navItem}
+              onClick={() => handleMobile("contact")}
+            >
+              Contact
             </li>
           </ul>
-        </nav>
-      </header>
+        </aside>
+      </ul>
+    </div>
+  );
+
+  return (
+    <header className={classes.header}>
+      <Link href="/">
+        <a title="anchor to home page">
+          <Logo />
+        </a>
+      </Link>
+      <nav className={classes.nav}>{width > breakpoint ? desktop : mobile}</nav>
+    </header>
   );
 }
 
